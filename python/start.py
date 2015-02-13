ac51c360e695a1f29408f8d95874428be4dc3cb1
洪斌 <hongbin@autohome.com.cn>
2015-02-13 10:51:42 +0800

    add python/start.py which is a template of python script

---
+++ python/start.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#@author ccljing                                                                                                           
+"""
+"""
+import sys,os,json
+local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+sys.path.append(local_path + "/./")
+def main():
+    pass
+if __name__ == ‘__main__’:
+    main()
